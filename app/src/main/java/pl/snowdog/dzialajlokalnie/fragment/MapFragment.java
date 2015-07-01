d276187e7316fe8a5a3d855ee048f9d7b4c62639
Bartek Kwiatkowski <bartholomaeus.kwiatkowski@gmail.com>
2015-07-01 09:06:24 +0200

    added init center on user animation on map

--- app/src/main/java/pl/snowdog/dzialajlokalnie/fragment/MapFragment.java
+++ app/src/main/java/pl/snowdog/dzialajlokalnie/fragment/MapFragment.java
@@ -1,27 +1,27 @@
 package pl.snowdog.dzialajlokalnie.fragment;
 
 
+import android.location.Location;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-
-import pl.snowdog.dzialajlokalnie.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends com.google.android.gms.maps.SupportMapFragment implements OnMapReadyCallback {
+public class MapFragment extends com.google.android.gms.maps.SupportMapFragment implements OnMapReadyCallback, GoogleMap.OnMyLocationChangeListener, GoogleMap.OnCameraChangeListener {
 
     private static final String TAG = "MapFragment";
     private GoogleMap map;
+    private boolean centerOnUser;
 
     public MapFragment() {
         // Required empty public constructor
@@ -31,6 +31,7 @@ public class MapFragment extends com.google.android.gms.maps.SupportMapFragment 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        centerOnUser = true;
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
@@ -43,7 +44,32 @@ public class MapFragment extends com.google.android.gms.maps.SupportMapFragment 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(52.4443618, 16.9460875), 14));
 
+        map.setMyLocationEnabled(true);
+        map.setBuildingsEnabled(true);
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(52.408333, 16.934167), 11));
+
+        map.setOnMyLocationChangeListener(this);
+        map.setOnCameraChangeListener(this);
+    }
+
+    @Override
+    public void onMyLocationChange(Location location) {
+        if (centerOnUser) {
+            LatLng currentLatLng = new LatLng(location.getLatitude(),
+                    location.getLongitude());
+
+            float zoom = map.getCameraPosition().zoom;
+            if (zoom < 12) {
+                zoom = 14;
+            }
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, zoom));
+        }
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition cameraPosition) {
+        centerOnUser = false;
     }
 }
