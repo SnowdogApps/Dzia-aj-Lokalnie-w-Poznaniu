c1b4c801df2645c21556ebb058e14a757a111b63
Bartek Kwiatkowski <bartholomaeus.kwiatkowski@gmail.com>
2015-07-01 17:12:49 +0200

    including layouts works only in root layout

--- app/src/main/java/pl/snowdog/dzialajlokalnie/fragment/IssuesFragment.java
+++ app/src/main/java/pl/snowdog/dzialajlokalnie/fragment/IssuesFragment.java
@@ -23,10 +23,19 @@ public class IssuesFragment extends ListFragment {
     protected void afterView() {
         List<Issue> issues = new ArrayList<>();
 
-        issues.add(new Issue(0, 0, 0, "Zepsuta ławka", "Bardzo zepsuta", 0, "Naramowicka 143", null, null, new Date(), 0, 2, "http://d.naszemiasto.pl/kadr/k/r/80/cb/51826a272c0d9_o,size,200x140,q,70,h,c775ac.jpg"));
+        // TODO remove temp data
+        issues.add(new Issue(0, 0, 0, "Łaszegerege", "Co mówi kabel do kabla?\n" +
+                "- Bądźmy w kontakcie!", 0, "Naramowicka 143", null, null, new Date(), 4, 1, "http://nie ma takiego lpiku.jpg"));
+        issues.add(new Issue(0, 0, 0, "Zepsuta ławka", "Bardzo zepsuta", 0, "Naramowicka 143", null, null, new Date(), 32, 2, "http://d.naszemiasto.pl/kadr/k/r/80/cb/51826a272c0d9_o,size,200x140,q,70,h,c775ac.jpg"));
         issues.add(new Issue(0, 0, 0, "Grill osiedlowy na ławce dla wszystkich pamperków", "Facet kupił fajny, duży telewizor, przyniósł do domu, żona patrzy, na pudle sporo jakichś znaczków informacyjnych. Zaciekawiona pyta:\n" +
                 "- Kochanie, co oznacza ta szklanka na opakowaniu?\n" +
-                "- To znaczy, że zakup trzeba opić.", 0, "Naramowicka 143", null, null, new Date(), 0, 5, "http://i.wp.pl/a/f/jpeg/29034/thstck_grill_jedzenie_600.jpeg"));
+                "- To znaczy, że zakup trzeba opić.", 0, "Naramowicka 143", null, null, new Date(), 178, 57, "http://i.wp.pl/a/f/jpeg/29034/thstck_grill_jedzenie_600.jpeg"));
+        issues.add(new Issue(0, 0, 0, "NOINSOS V ( ENIEW BIW EOUFE OWE", "Co mówi kabel do kabla?\n" +
+                "- Bądźmy w kontakcie!", 0, "Naramowicka 143", null, null, new Date(), 4723, 0, "http://nie ma takiego lpiku.jpg"));
+        issues.add(new Issue(0, 0, 0, "Zepsuta ławka", "Bardzo zepsuta", 0, "Naramowicka 143", null, null, new Date(), 32, 2, "http://d.naszemiasto.pl/kadr/k/r/80/cb/51826a272c0d9_o,size,200x140,q,70,h,c775ac.jpg"));
+        issues.add(new Issue(0, 0, 0, "Grill osiedlowy na ławce dla wszystkich pamperków", "Facet kupił fajny, duży telewizor, przyniósł do domu, żona patrzy, na pudle sporo jakichś znaczków informacyjnych. Zaciekawiona pyta:\n" +
+                "- Kochanie, co oznacza ta szklanka na opakowaniu?\n" +
+                "- To znaczy, że zakup trzeba opić.", 0, "Naramowicka 143", null, null, new Date(), 178, 57, "http://i.wp.pl/a/f/jpeg/29034/thstck_grill_jedzenie_600.jpeg"));
 
         adapter = new IssuesAdapter(issues);
         recyclerView.setAdapter(adapter);
