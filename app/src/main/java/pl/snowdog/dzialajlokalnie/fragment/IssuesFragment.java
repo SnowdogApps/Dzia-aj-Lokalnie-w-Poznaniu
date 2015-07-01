483fca8781ce27b2b58e7a36afb3ff9e1a043aee
Bartek Kwiatkowski <bartholomaeus.kwiatkowski@gmail.com>
2015-07-01 16:09:05 +0200

    added plurals and fixed items

--- app/src/main/java/pl/snowdog/dzialajlokalnie/fragment/IssuesFragment.java
+++ app/src/main/java/pl/snowdog/dzialajlokalnie/fragment/IssuesFragment.java
@@ -23,7 +23,10 @@ public class IssuesFragment extends ListFragment {
     protected void afterView() {
         List<Issue> issues = new ArrayList<>();
 
-        issues.add(new Issue(0, 0, 0, "Zepsuta ławka", "Bardzo zepsuta", 0, "Naramowicka 143", null, null, new Date(), 0, 0, null));
+        issues.add(new Issue(0, 0, 0, "Zepsuta ławka", "Bardzo zepsuta", 0, "Naramowicka 143", null, null, new Date(), 0, 2, null));
+        issues.add(new Issue(0, 0, 0, "Grill osiedlowy na ławce dla wszystkich pamperków", "Facet kupił fajny, duży telewizor, przyniósł do domu, żona patrzy, na pudle sporo jakichś znaczków informacyjnych. Zaciekawiona pyta:\n" +
+                "- Kochanie, co oznacza ta szklanka na opakowaniu?\n" +
+                "- To znaczy, że zakup trzeba opić.", 0, "Naramowicka 143", null, null, new Date(), 0, 5, null));
 
         adapter = new IssuesAdapter(issues);
         recyclerView.setAdapter(adapter);
