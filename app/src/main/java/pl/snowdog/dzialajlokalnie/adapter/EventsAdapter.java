5954618b758680c9e1ccb949816c92d4ba3f2786
Bartek Kwiatkowski <bartholomaeus.kwiatkowski@gmail.com>
2015-07-07 14:18:47 +0200

    EventsAdapter added

---
+++ app/src/main/java/pl/snowdog/dzialajlokalnie/adapter/EventsAdapter.java
@@ -0,0 +1,59 @@
+package pl.snowdog.dzialajlokalnie.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import pl.snowdog.dzialajlokalnie.R;
+import pl.snowdog.dzialajlokalnie.databinding.ItemEventBinding;
+import pl.snowdog.dzialajlokalnie.databinding.ItemIssueBinding;
+import pl.snowdog.dzialajlokalnie.model.Event;
+import pl.snowdog.dzialajlokalnie.model.Issue;
+
+/**
+ * Created by bartek on 01.07.15.
+ */
+public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder> {
+
+    private List<Event> events;
+
+    public EventsAdapter(List<Event> events) {
+        this.events = events;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        ItemEventBinding binding = ItemEventBinding.
+                inflate(LayoutInflater.from(viewGroup.getContext()));
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        Event event = events.get(i);
+        viewHolder.binding.setEvent(event);
+
+        Picasso.with(viewHolder.binding.getRoot().getContext()).load(event.getPhotoEventUri()).error(
+                R.drawable.ic_editor_insert_emoticon).into(viewHolder.binding.ivAvatar);
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ItemEventBinding binding;
+
+        public ViewHolder(ItemEventBinding binding) {
+            super(binding.getRoot());
+
+            this.binding = binding;
+        }
+    }
+}
--- app/src/main/java/pl/snowdog/dzialajlokalnie/model/Event.java
+++ app/src/main/java/pl/snowdog/dzialajlokalnie/model/Event.java
@@ -25,6 +25,9 @@ public class Event {
     private int commentsCount;
     private int votesCount;
 
+    public Event() {
+    }
+
     public Event(int eventID, int parentID, int userID, String title, String description, int districtID, double lon, double lat, String address, Date startDate, Date endDate, String facebookUrl, String eventUrl, Date createdAt, int eventStatus, int categoryID, String photoEventUri, int commentsCount, int votesCount) {
         this.eventID = eventID;
         this.parentID = parentID;
@@ -44,6 +47,158 @@ public class Event {
         this.categoryID = categoryID;
         this.photoEventUri = photoEventUri;
         this.commentsCount = commentsCount;
+        this.votesCount = votesCount;
+    }
+
+    public int getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(int eventID) {
+        this.eventID = eventID;
+    }
+
+    public int getParentID() {
+        return parentID;
+    }
+
+    public void setParentID(int parentID) {
+        this.parentID = parentID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDistrictID() {
+        return districtID;
+    }
+
+    public void setDistrictID(int districtID) {
+        this.districtID = districtID;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getFacebookUrl() {
+        return facebookUrl;
+    }
+
+    public void setFacebookUrl(String facebookUrl) {
+        this.facebookUrl = facebookUrl;
+    }
+
+    public String getEventUrl() {
+        return eventUrl;
+    }
+
+    public void setEventUrl(String eventUrl) {
+        this.eventUrl = eventUrl;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getEventStatus() {
+        return eventStatus;
+    }
+
+    public void setEventStatus(int eventStatus) {
+        this.eventStatus = eventStatus;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getPhotoEventUri() {
+        return photoEventUri;
+    }
+
+    public void setPhotoEventUri(String photoEventUri) {
+        this.photoEventUri = photoEventUri;
+    }
+
+    public int getCommentsCount() {
+        return commentsCount;
+    }
+
+    public void setCommentsCount(int commentsCount) {
+        this.commentsCount = commentsCount;
+    }
+
+    public int getVotesCount() {
+        return votesCount;
+    }
+
+    public void setVotesCount(int votesCount) {
         this.votesCount = votesCount;
     }
 }
