3d95d16a80d422453982a9f14941229b701ca426
Bartek Kwiatkowski <bartholomaeus.kwiatkowski@gmail.com>
2015-07-15 17:17:39 +0200

    added binding Issue in IssueFragment

--- app/src/main/java/pl/snowdog/dzialajlokalnie/fragment/IssueFragment.java
+++ app/src/main/java/pl/snowdog/dzialajlokalnie/fragment/IssueFragment.java
@@ -1,10 +1,19 @@
 package pl.snowdog.dzialajlokalnie.fragment;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.layouts.DataBindingInfo;
+import android.util.Log;
+import android.view.View;
+
+import com.squareup.picasso.Picasso;
+
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.FragmentArg;
+import org.androidannotations.annotations.ViewById;
 
 import pl.snowdog.dzialajlokalnie.R;
+import pl.snowdog.dzialajlokalnie.api.DlApi;
 import pl.snowdog.dzialajlokalnie.databinding.FragmentIssueBinding;
 import pl.snowdog.dzialajlokalnie.model.Issue;
 
@@ -15,20 +24,32 @@ import pl.snowdog.dzialajlokalnie.model.Issue;
 @EFragment(R.layout.fragment_issue)
 public class IssueFragment extends BaseFragment {
 
+    private static final String TAG = "IssueFragment";
     FragmentIssueBinding binding;
 
     @FragmentArg
     int objId;
 
+    @ViewById(R.id.issueDetails)
+    View rootView;
+
     @AfterViews
     void afterViews() {
-        binding = FragmentIssueBinding.inflate(getLayoutInflater(null));
+//        binding = FragmentIssueBinding.inflate(getLayoutInflater(null));
+        binding = DataBindingUtil.bind(rootView);
 
         getIssue(objId);
     }
 
     @Override
     protected void issueResult(Issue issue) {
+        
+        Log.d(TAG, "issueResult " + issue);
         binding.setIssue(issue);
+
+        Picasso.with(binding.getRoot().getContext()).
+                load(String.format(DlApi.PHOTO_THUMB_URL, issue.getPhotoIssueUri())).
+                error(R.drawable.ic_editor_insert_emoticon).
+                into(binding.ivAvatar);
     }
 }
