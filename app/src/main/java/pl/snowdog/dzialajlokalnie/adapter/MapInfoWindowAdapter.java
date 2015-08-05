8e2f03d90ee23120a9a6fb973e29275212519ab1
Bartek Kwiatkowski <bartholomaeus.kwiatkowski@gmail.com>
2015-08-05 15:21:42 +0200

    fixed MapInfoWindowAdapter

--- app/src/main/java/pl/snowdog/dzialajlokalnie/adapter/MapInfoWindowAdapter.java
+++ app/src/main/java/pl/snowdog/dzialajlokalnie/adapter/MapInfoWindowAdapter.java
@@ -20,6 +20,8 @@ import pl.snowdog.dzialajlokalnie.events.EventClickedEvent;
 import pl.snowdog.dzialajlokalnie.events.IssueClickedEvent;
 import pl.snowdog.dzialajlokalnie.model.Event;
 import pl.snowdog.dzialajlokalnie.model.Issue;
+import pl.snowdog.dzialajlokalnie.util.CircleTransform;
+import pl.snowdog.dzialajlokalnie.util.StringUtil;
 
 /**
  * Created by bartek on 14.07.15.
@@ -92,12 +94,24 @@ public class MapInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
             binding.setIssue(issue);
 
             Picasso.with(binding.getRoot().getContext()).
-                    load(String.format(DlApi.PHOTO_THUMB_URL, issue.getPhotoIssueUri())).
+                    load(String.format(DlApi.PHOTO_NORMAL_URL, issue.getPhotoIssueUri())).
                     error(R.drawable.ic_editor_insert_emoticon).
                     into(binding.ivAvatar);
 
+            Picasso.with(binding.getRoot().getContext()).
+                    load(String.format(DlApi.AVATAR_THUMB_URL, issue.getAuthorAvatar())).
+                    error(R.drawable.ic_editor_insert_emoticon).
+                    transform(new CircleTransform()).
+                    into(binding.ivAuthorAvatar);
+
+            //TODO problem with rendering affects missing photos
+            binding.ivAvatar.setVisibility(View.GONE);
+            binding.ivAuthorAvatar.setVisibility(View.GONE);
+
             binding.tvTitle.setText(issue.getTitle());
             binding.tvDesc.setText(issue.getDescription());
+            binding.tvUsername.setText(issue.getAuthorName());
+            binding.tvDate.setText(StringUtil.date2String(issue.getCreatedAt()));
             binding.ratingWidget.ibRateUp.setVisibility(View.INVISIBLE);
             binding.ratingWidget.ibRateDown.setVisibility(View.INVISIBLE);
             binding.ratingWidget.tvRating.setText(String.valueOf(issue.getIssueRating()));
@@ -110,6 +124,13 @@ public class MapInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
             stringBuilder.append(res.getString(R.string.tags, issue.getCategoriesText()));
             binding.footerWidget.tvFooter.setText(stringBuilder.toString());
 
+            stringBuilder = new StringBuilder();
+            stringBuilder.append(issue.getDistrictName());
+            stringBuilder.append(", ");
+            stringBuilder.append(issue.getAddress());
+
+            binding.footerWidget.tvFooter2.setText(stringBuilder.toString());
+
             //TODO problem with rendering because view is rendered as an image and does not update itself. Data binding won't work
             // https://developers.google.com/maps/documentation/android/infowindows#info_window_events
             return binding.getRoot();
@@ -121,12 +142,24 @@ public class MapInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
             binding.setEvent(event);
 
             Picasso.with(binding.getRoot().getContext()).
-                    load(String.format(DlApi.PHOTO_THUMB_URL, event.getPhotoEventUri())).
+                    load(String.format(DlApi.PHOTO_NORMAL_URL, event.getPhotoEventUri())).
                     error(R.drawable.ic_editor_insert_emoticon).
                     into(binding.ivAvatar);
 
+            Picasso.with(binding.getRoot().getContext()).
+                    load(String.format(DlApi.AVATAR_THUMB_URL, event.getAuthorAvatar())).
+                    error(R.drawable.ic_editor_insert_emoticon).
+                    transform(new CircleTransform()).
+                    into(binding.ivAuthorAvatar);
+
+            //TODO problem with rendering affects missing photos
+            binding.ivAvatar.setVisibility(View.GONE);
+            binding.ivAuthorAvatar.setVisibility(View.GONE);
+
             binding.tvTitle.setText(event.getTitle());
             binding.tvDesc.setText(event.getDescription());
+            binding.tvUsername.setText(event.getAuthorName());
+            binding.tvDate.setText(StringUtil.date2String(event.getCreatedAt()));
             binding.attendingWidget.ibAttend.setVisibility(View.INVISIBLE);
             binding.attendingWidget.tvCount.setText(String.valueOf(event.getAttendingCount()));
 
@@ -137,6 +170,13 @@ public class MapInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
             stringBuilder.append(", ");
             stringBuilder.append(res.getString(R.string.tags, event.getCategoriesText()));
             binding.footerWidget.tvFooter.setText(stringBuilder.toString());
+
+            stringBuilder = new StringBuilder();
+            stringBuilder.append(event.getDistrictName());
+            stringBuilder.append(", ");
+            stringBuilder.append(event.getAddress());
+
+            binding.footerWidget.tvFooter2.setText(stringBuilder.toString());
 
             return binding.getRoot();
         }
